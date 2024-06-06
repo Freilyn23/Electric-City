5bcb6974676dbf8490b7ca65a9edd497cdc05f45
Freilyn23 <alexpenacanel09@gmail.com>
2024-06-06 16:10:14 -0400

    Add files via upload

---
+++ DiseñoWeb/index.php
@@ -0,0 +1,19 @@
+<!--
+Pagina index donde se hacen los CTA o llamados a la accion de las demas paginas del proyecto.
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+<!--
+Llamados a la accion de las paginas de Inicio, Marcas y Quienes somos.
+-->
+    <?php include "views/CTAInicio.php"?>
+    <?php include "views/CTAMarcas.php"?>
+    <?php include "views/CTAQuienesSomos.php"?>
+</body>
+</html>
---
+++ DiseñoWeb/views/CTAQuienesSomos.php
@@ -0,0 +1,31 @@
+<!--
+Seccion de Quienes somos dentro de la pagina de inicio.
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--
+Conexion a las clases de Bootstrap del proyecto.
+-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="row" style="background-color:#000000; color:white ">
+        <div class="col-md-6" style="padding-left: 12%; padding-right: 12%; padding-top: 4% ; padding-bottom: 4%">
+            <h2 class="text-center">
+            ¿Quienes somos?
+            </h2>
+            <P>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.
+
+            </P>
+            <button class="btn btn-primary btn-lg"> Conoce Mas </button>
+        </div>
+        <div class="col-md-6" style="padding:4%"><img style="width:80%; height:200px; object-fit:cover" src="https://img.freepik.com/foto-gratis/co-trabajando-personas-trabajando-juntas_23-2149328346.jpg" alt="">
+    </div>
+    </div>
+</body>
+</html>
